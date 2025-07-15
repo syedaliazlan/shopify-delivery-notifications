071a278c370e716e01d9e4ac3f7323ce46306cd7
Ali Azlan <syd.aliazlan@gmail.com>
2025-07-15 05:03:04 +0100

    Added the webhook intialization across the app.

--- app/routes/init-webhooks.jsx
+++ app/routes/init-webhooks.jsx
@@ -1,12 +1,15 @@
+import { json } from "@remix-run/node";
 import { authenticate, registerWebhooks } from "../shopify.server";
 
 export const loader = async ({ request }) => {
   const { session } = await authenticate.admin(request);
 
-  // ✅ Register all webhooks after auth
   const result = await registerWebhooks({ session });
 
-  console.log("✅ Webhooks registered:", result);
+  console.log("✅ Manual Webhook Init Result:", result);
 
-  return null;
+  return json({
+    status: "Webhooks registered",
+    details: result,
+  });
 };
